c0d57cf546c987ea5278d4a5f16d4ad9b96f24fa
Ritika <ritikaroy7272@gmail.com>
2025-12-24 12:33:58 -0500

    Dec 24 Commit

---
+++ flaskApp.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///uniforms.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# -------- MODEL --------
+class UniformItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_type = db.Column(db.String(50))
+    size = db.Column(db.String(20))
+    condition = db.Column(db.String(20))
+    status = db.Column(db.String(20))
+
+
+# -------- ROUTES --------
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/inventory")
+def inventory():
+    items = UniformItem.query.all()
+    return render_template("inventory.html", items=items)
+
+
+# -------- APP START --------
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()   # ← THIS CREATES uniforms.db
+    app.run(debug=True)
