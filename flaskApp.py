c107083c59ecf1a7165bdac0a8f5c5a12799cfcd
Ritika <ritikaroy7272@gmail.com>
2026-05-26 21:30:09 -0400

    Changed background

--- flaskApp.py
+++ flaskApp.py
@@ -666,6 +666,5 @@ def approve_requests():
             reqs = list(csv.DictReader(f))
     return render_template("approve.html", requests=reqs)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
